ba000160bb39c2bb3418c82cca50ffbb70da0a7d
Guilherme Cesar Alves de Andrade <guilherme.alves@labs.dcx>
2023-06-01 11:37:38 -0300

    Programa Guilherme Cesar

---
+++ src/GuilhermeCesar.java
@@ -0,0 +1,10 @@
+public class GuilhermeCesar {
+    public int numero1;
+    public int numero2;
+    public int total;
+
+    public int soma(int numero1, int numero2){
+        return total = numero1 + numero2;
+    }
+
+}
